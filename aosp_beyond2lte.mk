cb95eddeb98fe3e06a9b88ac4dbd731a5c5b359a
FreeRunner4ever <ledrew101806@gmail.com>
2023-01-15 12:01:09 -0500

    aosp bringup

--- aosp_beyond2lte.mk
+++ aosp_beyond2lte.mk
@@ -37,12 +37,12 @@ TARGET_SCREEN_WIDTH := 1440
 EXTRA_UDFPS_ANIMATIONS := true
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := cherish_beyond2lte
+PRODUCT_NAME := aosp_beyond2lte
 PRODUCT_DEVICE := beyond2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G975F
